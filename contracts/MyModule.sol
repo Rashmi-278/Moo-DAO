e633f3491f1ded8f538705736213fe90948cdd53
Rashmi-278 <rashmiabbigeri@gmail.com>
2021-09-09 00:54:38 +0530

    init

---
+++ contracts/MyModule.sol
@@ -0,0 +1,9 @@
+//SPDX-license-Identifier: Unlicense
+pragma solidity ^0.8.6;
+
+import "@gnosis/zodiac/contracts/core/Module.sol";
+
+contract MyModule is Module {
+  /// insert your code here
+}
+
